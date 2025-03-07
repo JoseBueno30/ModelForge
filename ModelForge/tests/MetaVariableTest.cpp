99d8bf820133ce110903ce156b724adeb787fa80
JoseBueno30 <124668506+JoseBueno30@users.noreply.github.com>
2025-03-07 09:59:21 +0100

    MetaVariableTest class

---
+++ ModelForge/tests/MetaVariableTest.cpp
@@ -0,0 +1,59 @@
+#include <QTest>
+
+#include <metamodel/MetaVariable.h>
+
+class MetaVariableTest: public QObject
+{
+    Q_OBJECT
+
+private:
+    MetaModel::MetaVariable* metaVariable;
+    std::shared_ptr<MetaModel::MetaType> metaType;
+
+private slots:
+    void init();
+
+    void metaVariable_getName_returnsCorrectName();
+    void metaVariable_setName_updatesName();
+    void metaVariable_getType_returnsCorrectType();
+    void metaVariable_setType_updatesType();
+
+};
+
+void MetaVariableTest::init() {
+    metaType = MetaModel::String::instance();
+
+    metaVariable = new MetaModel::MetaVariable("TestVariable", metaType);
+}
+
+void MetaVariableTest::metaVariable_getName_returnsCorrectName(){
+    QCOMPARE("TestVariable", metaVariable->getName());
+}
+
+void MetaVariableTest::metaVariable_setName_updatesName(){
+    std::string newName = "NewName";
+
+    metaVariable->setName(newName);
+
+    QCOMPARE(newName, metaVariable->getName());
+}
+
+void MetaVariableTest::metaVariable_getType_returnsCorrectType(){
+    auto stringType = MetaModel::String::instance();
+
+    QVERIFY(metaVariable->getType().equals(*stringType));
+}
+
+void MetaVariableTest::metaVariable_setType_updatesType(){
+    auto stringType = MetaModel::String::instance();
+    auto newType = MetaModel::Integer::instance();
+
+    metaVariable->setType(newType);
+
+    QVERIFY(metaVariable->getType().equals(*newType));
+    QVERIFY(!metaVariable->getType().equals(*stringType));
+}
+
+
+QTEST_MAIN(MetaVariableTest)
+#include "MetaVariableTest.moc"
