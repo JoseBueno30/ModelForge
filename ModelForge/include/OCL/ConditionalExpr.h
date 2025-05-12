8d208c659b9994d16700757aabed3288d097a502
JoseBueno30 <124668506+JoseBueno30@users.noreply.github.com>
2025-05-12 18:14:29 +0200

    conditionalExpr buildExprString()

--- ModelForge/include/OCL/ConditionalExpr.h
+++ ModelForge/include/OCL/ConditionalExpr.h
@@ -9,6 +9,7 @@ protected:
     std::shared_ptr<Expr> ifExpr;
     std::shared_ptr<Expr> thenExpr;
     std::shared_ptr<Expr> elseExpr;
+    std::string buildExprString() const override;
 public:
     ConditionalExpr(const std::string& expression, const bool isComplex, const std::shared_ptr<MetaType>& type,
                     const std::shared_ptr<Expr>& ifExpr, const std::shared_ptr<Expr>& thenExpr, const std::shared_ptr<Expr>& elseExpr);
